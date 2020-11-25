e80d2d65066463e3a29b0b361260868ddb3949fc
Elmahdi KARBAL <56792446+mekarbal@users.noreply.github.com>
2020-11-25 18:16:25 +0100

    Crud operations using c# and sql server

---
+++ CRUD_App_csharp/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace CRUD_App_csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.prenom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.telephone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.adresse = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.studentDataGridView = new System.Windows.Forms.DataGridView();
+            this.comboBox_pays = new System.Windows.Forms.ComboBox();
+            this.comboBox_ville = new System.Windows.Forms.ComboBox();
+            this.comboBox_specialite = new System.Windows.Forms.ComboBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.studentDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom";
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(126, 91);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(201, 20);
+            this.nom.TabIndex = 1;
+            this.nom.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(34, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // prenom
+            // 
+            this.prenom.Location = new System.Drawing.Point(126, 147);
+            this.prenom.Name = "prenom";
+            this.prenom.Size = new System.Drawing.Size(201, 20);
+            this.prenom.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prénom";
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(126, 207);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(201, 20);
+            this.email.TabIndex = 6;
+            this.email.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.email_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // telephone
+            // 
+            this.telephone.Location = new System.Drawing.Point(126, 264);
+            this.telephone.Name = "telephone";
+            this.telephone.Size = new System.Drawing.Size(201, 20);
+            this.telephone.TabIndex = 8;
+            this.telephone.TextChanged += new System.EventHandler(this.telephone_TextChanged);
+            this.telephone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.telephone_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Télephone";
+            // 
+            // adresse
+            // 
+            this.adresse.Location = new System.Drawing.Point(545, 88);
+            this.adresse.Name = "adresse";
+            this.adresse.Size = new System.Drawing.Size(201, 20);
+            this.adresse.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(449, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Adresse";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(449, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Pays";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(449, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 18);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Ville";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(449, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Spècialité";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(558, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 38);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Vider";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(385, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 38);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Supprimer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(203, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 38);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Mise à jour";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // studentDataGridView
+            // 
+            this.studentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentDataGridView.Location = new System.Drawing.Point(34, 408);
+            this.studentDataGridView.Name = "studentDataGridView";
+            this.studentDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.studentDataGridView.Size = new System.Drawing.Size(779, 189);
+            this.studentDataGridView.TabIndex = 17;
+            this.studentDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.studentDataGridView_CellClick);
+            this.studentDataGridView.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.studentDataGridView_KeyPress);
+            // 
+            // comboBox_pays
+            // 
+            this.comboBox_pays.AccessibleName = "";
+            this.comboBox_pays.FormattingEnabled = true;
+            this.comboBox_pays.Items.AddRange(new object[] {
+            "Maroc",
+            "France",
+            "Allemand"});
+            this.comboBox_pays.Location = new System.Drawing.Point(545, 146);
+            this.comboBox_pays.Name = "comboBox_pays";
+            this.comboBox_pays.Size = new System.Drawing.Size(201, 21);
+            this.comboBox_pays.TabIndex = 18;
+            this.comboBox_pays.SelectedIndexChanged += new System.EventHandler(this.comboBox_pays_SelectedIndexChanged);
+            // 
+            // comboBox_ville
+            // 
+            this.comboBox_ville.FormattingEnabled = true;
+            this.comboBox_ville.Location = new System.Drawing.Point(545, 206);
+            this.comboBox_ville.Name = "comboBox_ville";
+            this.comboBox_ville.Size = new System.Drawing.Size(201, 21);
+            this.comboBox_ville.TabIndex = 19;
+            this.comboBox_ville.SelectedIndexChanged += new System.EventHandler(this.comboBox_ville_SelectedIndexChanged);
+            // 
+            // comboBox_specialite
+            // 
+            this.comboBox_specialite.FormattingEnabled = true;
+            this.comboBox_specialite.Items.AddRange(new object[] {
+            "C#",
+            "JEE",
+            "Back-End & Front-End"});
+            this.comboBox_specialite.Location = new System.Drawing.Point(545, 263);
+            this.comboBox_specialite.Name = "comboBox_specialite";
+            this.comboBox_specialite.Size = new System.Drawing.Size(201, 21);
+            this.comboBox_specialite.TabIndex = 20;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(847, 630);
+            this.Controls.Add(this.comboBox_specialite);
+            this.Controls.Add(this.comboBox_ville);
+            this.Controls.Add(this.comboBox_pays);
+            this.Controls.Add(this.studentDataGridView);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.adresse);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.telephone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.prenom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nom);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Informations";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.studentDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nom;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox prenom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox telephone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox adresse;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView studentDataGridView;
+        private System.Windows.Forms.ComboBox comboBox_pays;
+        private System.Windows.Forms.ComboBox comboBox_ville;
+        private System.Windows.Forms.ComboBox comboBox_specialite;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
+
---
+++ CRUD_App_csharp/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace CRUD_App_csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=students;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public int studentId;
+        bool valideEmail = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllStudents();
+        }
+
+        private void GetAllStudents()
+        {
+
+            SqlCommand query = new SqlCommand("SELECT * From apprenant", conn);
+            DataTable dt = new DataTable();
+
+            conn.Open();
+            SqlDataReader sdr = query.ExecuteReader();
+            dt.Load(sdr);
+            conn.Close();
+            studentDataGridView.DataSource = dt;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isValid() && isValidEmail(email.Text))
+            {
+                SqlCommand query = new SqlCommand("INSERT INTO apprenant VALUES (@nom,@prenom,@email,@telephone,@adresse,@pays,@ville,@specialite)", conn);
+                query.CommandType = CommandType.Text;
+
+                query.Parameters.AddWithValue("@nom", nom.Text);
+                query.Parameters.AddWithValue("@prenom",prenom.Text);
+                query.Parameters.AddWithValue("@email", email.Text);
+                query.Parameters.AddWithValue("@telephone", int.Parse(telephone.Text)) ;
+                query.Parameters.AddWithValue("@adresse", adresse.Text);
+                query.Parameters.AddWithValue("@pays",comboBox_pays.Text);
+                query.Parameters.AddWithValue("@ville", comboBox_ville.Text);
+                query.Parameters.AddWithValue("@specialite", comboBox_specialite.Text);
+
+
+                conn.Open();
+                query.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("Nouveau aprenant a ete bien ajouter", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                clearAllFields();
+
+                GetAllStudents();
+
+            }
+        }
+
+        private void clearAllFields()
+        {
+            nom.Clear();
+            prenom.Clear();
+            email.Clear();
+            telephone.Clear();
+            adresse.Clear();
+            comboBox_pays.ResetText();
+            comboBox_ville.ResetText();
+            comboBox_specialite.ResetText();
+            nom.Focus();
+
+        }
+
+       
+
+        private bool isValid()
+        {
+            if (nom.Text == string.Empty || prenom.Text==string.Empty ||email.Text==string.Empty || telephone.Text==string.Empty ||adresse.Text==string.Empty || comboBox_pays.Text==string.Empty ||comboBox_ville.Text==string.Empty ||  comboBox_specialite.Text==string.Empty)
+            {
+                MessageBox.Show("Veuillez Remplire tout les champs avant l\'insertion  ","Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+
+        }
+
+        private void comboBox_ville_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void comboBox_pays_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox_pays.Text == "Maroc")
+            {
+                comboBox_ville.Items.Clear();
+                comboBox_ville.Items.Add("Safi");
+                comboBox_ville.Items.Add("Agadir");
+                comboBox_ville.Items.Add("Esaouira");
+                comboBox_ville.Items.Add("Titouan");
+            }
+            else if (comboBox_pays.Text == "France")
+            {
+                comboBox_ville.Items.Clear();
+                comboBox_ville.Items.Add("Paris");
+                comboBox_ville.Items.Add("Lyon");
+                comboBox_ville.Items.Add("Toulouse");
+                comboBox_ville.Items.Add("Tours");
+            }
+            else if (comboBox_pays.Text == "Allemand")
+            {
+                comboBox_ville.Items.Clear();
+                comboBox_ville.Items.Add("Bonn");
+                comboBox_ville.Items.Add("Berlin");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clearAllFields();
+        }
+
+        private void studentDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+             
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (studentId > 0)
+            {
+                SqlCommand query = new SqlCommand("UPDATE apprenant SET nom=@nom,prenom=@prenom,email=@email,telephone=@telephone,adrese=@adresse,pays=@pays,ville=@ville,choixSpecialite=@specialite WHERE id=@ID", conn);
+                query.CommandType = CommandType.Text;
+
+                query.Parameters.AddWithValue("@nom", nom.Text);
+                query.Parameters.AddWithValue("@prenom", prenom.Text);
+                query.Parameters.AddWithValue("@email", email.Text);
+                query.Parameters.AddWithValue("@telephone", telephone.Text);
+                query.Parameters.AddWithValue("@adresse", adresse.Text);
+                query.Parameters.AddWithValue("@pays", comboBox_pays.Text);
+                query.Parameters.AddWithValue("@ville", comboBox_ville.Text);
+                query.Parameters.AddWithValue("@specialite", comboBox_specialite.Text);
+                query.Parameters.AddWithValue("@ID",this.studentId);
+
+
+                conn.Open();
+                query.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("Mise a jour  a ete bien fait", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                clearAllFields();
+
+                GetAllStudents();
+            }
+            else
+            {
+                MessageBox.Show("S\'il vous plait selectionnez un champs ", "Selectionne?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void studentDataGridView_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void studentDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            studentId = Convert.ToInt32(studentDataGridView.SelectedRows[0].Cells[0].Value);
+            nom.Text = studentDataGridView.SelectedRows[0].Cells[1].Value.ToString();
+            prenom.Text = studentDataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            email.Text = studentDataGridView.SelectedRows[0].Cells[3].Value.ToString();
+            telephone.Text = studentDataGridView.SelectedRows[0].Cells[4].Value.ToString();
+            adresse.Text = studentDataGridView.SelectedRows[0].Cells[5].Value.ToString();
+            comboBox_pays.Text = studentDataGridView.SelectedRows[0].Cells[6].Value.ToString();
+            comboBox_ville.Text = studentDataGridView.SelectedRows[0].Cells[7].Value.ToString();
+            comboBox_specialite.Text = studentDataGridView.SelectedRows[0].Cells[8].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (studentId > 0)
+            {
+                SqlCommand query = new SqlCommand("DELETE FROM apprenant WHERE id=@ID", conn);
+                query.CommandType = CommandType.Text;
+
+                query.Parameters.AddWithValue("@ID", this.studentId);
+
+
+                conn.Open();
+                query.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("un champs a ete bien supprimer", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                clearAllFields();
+
+                GetAllStudents();
+            }
+            else
+            {
+                MessageBox.Show("Selectionnez un  champs s\'il vous plait", "DELETE?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void telephone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                errorProvider1.SetError(this.telephone, "just numbers please");
+                
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+
+        private void email_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.isValidEmail(nom.Text);
+        }
+
+        private bool isValidEmail(string email)
+        {
+            Regex check = new Regex(@"^\w+[\w-.]+@\w{5}.[a-z]{2,3}$");
+
+            valideEmail = check.IsMatch(email);
+
+            if (valideEmail == true)
+            {
+                errorProvider1.Clear();
+                return true;
+            }
+            else
+            {
+                errorProvider1.SetError(this.email, "please provide valid email address");
+                return false;
+            }
+        }
+
+        private void telephone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
